6d6c5d590008e9310f5c8a85fc90038a9e7373a5
Andrew <90206611+AndreiAlesik@users.noreply.github.com>
2023-11-06 21:19:00 +0100

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -8,21 +8,23 @@ current_decade = None
 current_genre_set = set()
 count = 0
 
+def emit_result(label, artist, decade, count, genres):
+    genres_list = ','.join(genres)
+    print("{}\t{}\t{}\t{}\t{}\t{}".format(label, artist, current_artist_name, decade, count, genres_list))
+
 for line in sys.stdin:
-    label_id, artist_id, artist_name, decade, genre, _ = line.strip().split("\t")
-    key = (label_id, artist_id, artist_name, decade)
+    label_id, artist_id, current_artist_name, decade, genre, _ = line.strip().split("\t")
+    key = (label_id, artist_id, current_artist_name, decade)
     
     if (current_label, current_artist, current_decade) == key[:3]:
         current_genre_set.add(genre)
         count += 1
     else:
         if current_label:
-            genres_list = ','.join(current_genre_set)
-            print("{}\t{}\t{}\t{}\t{}".format(current_label, current_artist, current_decade, count, genres_list))
-        current_label, current_artist, current_decade = key[:3]
+            emit_result(current_label, current_artist, current_decade, count, current_genre_set)
+        current_label, current_artist, current_artist_name, current_decade = key
         current_genre_set = set([genre])
         count = 1
 
 if current_label:
-    genres_list = ','.join(current_genre_set)
-    print("{}\t{}\t{}\t{}\t{}".format(current_label, current_artist, current_decade, count, genres_list))
+    emit_result(current_label, current_artist, current_decade, count, current_genre_set)
