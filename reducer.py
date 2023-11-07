56a9f3a0a18ac044ae62cf5e4ae430b1f16bfc6c
Andrew <90206611+AndreiAlesik@users.noreply.github.com>
2023-11-07 22:11:01 +0100

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -5,19 +5,19 @@ from collections import defaultdict
 # Словарь для подсчета количества пластинок и жанров
 label_artist_decade = defaultdict(lambda: {'count': 0, 'genres': set()})
 
-current_key = None
-
 for line in sys.stdin:
-    # Удаление лишних пробельных символов
-    line = line.strip()
+    line = line.strip()  # Удаление лишних пробельных символов
     # Разбиваем строку на ключ и значение
-    label_id, artist_id, artist_name, decade, genre = line.split('\t')
+    label_id, artist_id, artist_name, decade, genre, count = line.split('\t')
     
+    # Приводим count к целому числу
+    count = int(count)
+
     # Создаем ключ для словаря
     key = (label_id, artist_id, artist_name, decade)
     
     # Считаем количество пластинок и добавляем жанры в множество
-    label_artist_decade[key]['count'] += 1
+    label_artist_decade[key]['count'] += count
     label_artist_decade[key]['genres'].add(genre)
 
 # Выводим результаты
@@ -25,5 +25,6 @@ for key, value in label_artist_decade.items():
     label_id, artist_id, artist_name, decade = key
     count = value['count']
     genres = list(value['genres'])
-    print(f'{label_id}\t{artist_id}\t{artist_name}\t{decade}\t{count}\t{genres}')
-
+    # Форматируем вывод жанров как строку, разделенную запятыми
+    genres_str = ','.join(genres)
+    print("{}\t{}\t{}\t{}\t{}\t{}".format(label_id, artist_id, artist_name, decade, count, genres_str))
