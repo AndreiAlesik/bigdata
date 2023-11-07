f7a1bb2c5e4b48fb9177a13b505598f9ed6b1a66
Andrew <90206611+AndreiAlesik@users.noreply.github.com>
2023-11-07 22:03:05 +0100

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -2,23 +2,31 @@
 import sys
 
 for line in sys.stdin:
-    # Удаление лишних пробельных символов
-    line = line.strip()
-    # Разбиваем строку на поля
-    fields = line.split('\u0001')
+    line = line.strip()  # Удаление лишних пробельных символов
+    fields = line.split('\u0001')  # Разбиваем строку на поля
     
-    # Извлекаем необходимые поля
-    label_id = fields[6]
-    artist_id = fields[4]
-    artist_name = fields[5]
-    genre = fields[8]
-    release_date = fields[11]
-    
-    # Вычисляем декаду
-    decade = None
-    if release_date:
-        decade = (int(release_date[:4]) // 10) * 10
-    
-    # Выводим ключ (label_id, artist_id, decade) и значение (genre)
-    if label_id and artist_id and decade:
-        print(f'{label_id}\t{artist_id}\t{artist_name}\t{decade}\t{genre}')
+    # Убедимся, что у нас достаточно полей
+    if len(fields) >= 12:
+        label_id = fields[6]
+        artist_id = fields[4]
+        artist_name = fields[5]
+        genre = fields[8]
+        release_date = fields[11]
+
+        # Используем обработку исключений для преобразования даты
+        try:
+            # Вычисляем декаду
+            if release_date:
+                decade = (int(release_date[:4]) // 10) * 10
+            else:
+                # Если дата не указана, пропускаем эту запись
+                continue
+
+            # Выводим ключ (label_id, artist_id, decade) и значение (genre)
+            print(f'{label_id}\t{artist_id}\t{artist_name}\t{decade}\t{genre}')
+        except ValueError:
+            # Если возникла ошибка преобразования, просто пропустим эту строку
+            continue
+    else:
+        # Если полей не достаточно, пропустим эту строку
+        continue
